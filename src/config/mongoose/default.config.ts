31ef04c5aefa20284ef6988a906216d2f59ce7d9
Krzysztof-Nerwinski <k.nerwinski@gmail.com>
2022-11-17 18:34:35 +0100

    fix: remove logging of env variables;

--- src/config/mongoose/default.config.ts
+++ src/config/mongoose/default.config.ts
@@ -1,7 +1,5 @@
 const { MONGO_USER, MONGO_PASSWORD, DEFAULT_URL, MONGO_DB_NAME } = process.env;
 
-console.log('envs', { MONGO_USER, MONGO_PASSWORD, DEFAULT_URL, MONGO_DB_NAME });
-
 export default {
     id: 'default',
     url: DEFAULT_URL || 'mongodb://localhost:27017/default',
